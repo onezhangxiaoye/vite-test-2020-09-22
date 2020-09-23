60eba5f630e716bd8ca2d6e678de7e308dee12b7
yuehaiming <915873514@qq.com>
2020-09-23 17:03:34 +0800

    feat docs

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+
+
+// vite.config.js
+// https://github.com/vitejs/vite/blob/master/src/node/config.ts
+// https://github.com/vitejs/vite#config-file
+export default {
+    // 选项...
+    base: './',
+    outDir: 'docs',
+    assetsDir: '',
+}
