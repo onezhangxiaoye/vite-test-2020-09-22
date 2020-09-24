8e4963a0a461594ad4ecb3d9e6defe309d2fd424
yuehaiming <915873514@qq.com>
2020-09-24 11:12:12 +0800

    feat 图片加载等待

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="main-app" :style="mainStyle">
+    <img alt="Vue logo" src="../public/assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue";
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  setup(){
+    let mainStyle = ref('');
+    let img = new Image();
+    img.src = '/assets/bg.webp';
+    img.onload = function () {
+      console.log('加载完成');
+      mainStyle.value = 'background-image: url("/assets/bg.webp");';
+    }
+    return {
+      mainStyle,
+    }
+  },
 }
 </script>
+<style>
+  .main-app{
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+    background-position: center center;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
