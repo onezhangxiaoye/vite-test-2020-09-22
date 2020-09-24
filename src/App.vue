76abf46af0beaadd9c594d41075ea175b330ff4a
yuehaiming <915873514@qq.com>
2020-09-24 11:43:53 +0800

    加载效果

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="main-app" :style="mainStyle">
+  <div class="main-app" v-if="visible">
+    <div class="main-app-loading"></div>
+  </div>
+  <div class="main-app" :style="mainStyle" v-else>
     <img alt="Vue logo" src="/logo.png" />
     <HelloWorld msg="Hello Vue 3.0 + Vite" />
   </div>
@@ -16,14 +19,17 @@ export default {
   },
   setup(){
     let mainStyle = ref('');
+    let visible = ref(true);
     let img = new Image();
     img.src = './bg.webp';
     img.onload = function () {
       console.log('加载完成');
+      visible.value = false;
       mainStyle.value = 'background-image: url("./bg.webp");';
     }
     return {
       mainStyle,
+      visible,
     }
   },
 }
@@ -35,5 +41,27 @@ export default {
     background-position: center center;
     width: 100vw;
     height: 100vh;
+    position: relative;
+  }
+  .main-app-loading{
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-color: black;
+    animation-name: xua-zhuan;
+    animation-duration: .2s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+
+  }
+  @keyframes xua-zhuan {
+    from{
+      transform: rotate(0deg);
+    }
+    to{
+      transform: rotate(360deg);
+    }
   }
 </style>
